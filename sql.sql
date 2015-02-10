6cd2f1b23ff0e63b20040f1020df6e165d8ce621
Someant <594272732@qq.com>
2015-02-10 14:27:54 +0800

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,48 +1,52 @@
-===数据库 shad
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2015 年 02 月 10 日 06:27
+-- 服务器版本: 5.5.24-log
+-- PHP 版本: 5.4.3
 
-== 表的结构 user
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-|------
-|字段|类型|空|默认
-|------
-|//**id**//|int(11)|否|
-|email|varchar(32)|否|
-|pass|varchar(32)|否|
-|passwd|varchar(16)|否|
-|t|int(11)|否|0
-|u|bigint(20)|否|
-|d|bigint(20)|否|
-|transfer_enable|bigint(20)|否|
-|//**port**//|int(11)|否|
-|switch|tinyint(4)|否|1
-|enable|tinyint(4)|否|1
-|type|tinyint(4)|否|1
-|last_get_gitf_time|int(11)|否|0
-|last_rest_pass_time|int(11)|否|0
-|vpnuser|varchar(20)|否|
-|vpnpass|varchar(20)|否|
-|endtime|date|否|
-== 转存表中的数据 user
 
-== 表的结构 user
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-|------
-|字段|类型|空|默认
-|------
-|//**id**//|int(11)|否|
-|email|varchar(32)|否|
-|pass|varchar(32)|否|
-|passwd|varchar(16)|否|
-|t|int(11)|否|0
-|u|bigint(20)|否|
-|d|bigint(20)|否|
-|transfer_enable|bigint(20)|否|
-|//**port**//|int(11)|否|
-|switch|tinyint(4)|否|1
-|enable|tinyint(4)|否|1
-|type|tinyint(4)|否|1
-|last_get_gitf_time|int(11)|否|0
-|last_rest_pass_time|int(11)|否|0
-|vpnuser|varchar(20)|否|
-|vpnpass|varchar(20)|否|
-|endtime|date|否|
+--
+-- 数据库: `shad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(32) NOT NULL,
+  `pass` varchar(32) NOT NULL,
+  `passwd` varchar(16) NOT NULL,
+  `t` int(11) NOT NULL DEFAULT '0',
+  `u` bigint(20) NOT NULL,
+  `d` bigint(20) NOT NULL,
+  `transfer_enable` bigint(20) NOT NULL,
+  `port` int(11) NOT NULL,
+  `switch` tinyint(4) NOT NULL DEFAULT '1',
+  `enable` tinyint(4) NOT NULL DEFAULT '1',
+  `type` tinyint(4) NOT NULL DEFAULT '1',
+  `last_get_gitf_time` int(11) NOT NULL DEFAULT '0',
+  `last_rest_pass_time` int(11) NOT NULL DEFAULT '0',
+  `vpnuser` varchar(20) NOT NULL,
+  `vpnpass` varchar(20) NOT NULL,
+  `endtime` date NOT NULL,
+  PRIMARY KEY (`id`,`port`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=39 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
