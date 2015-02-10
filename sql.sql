3c26cadc821e363acc2a04d6ce3744391dbff683
Someant <594272732@qq.com>
2015-02-10 14:26:52 +0800

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,48 @@
+===数据库 shad
+
+== 表的结构 user
+
+|------
+|字段|类型|空|默认
+|------
+|//**id**//|int(11)|否|
+|email|varchar(32)|否|
+|pass|varchar(32)|否|
+|passwd|varchar(16)|否|
+|t|int(11)|否|0
+|u|bigint(20)|否|
+|d|bigint(20)|否|
+|transfer_enable|bigint(20)|否|
+|//**port**//|int(11)|否|
+|switch|tinyint(4)|否|1
+|enable|tinyint(4)|否|1
+|type|tinyint(4)|否|1
+|last_get_gitf_time|int(11)|否|0
+|last_rest_pass_time|int(11)|否|0
+|vpnuser|varchar(20)|否|
+|vpnpass|varchar(20)|否|
+|endtime|date|否|
+== 转存表中的数据 user
+
+== 表的结构 user
+
+|------
+|字段|类型|空|默认
+|------
+|//**id**//|int(11)|否|
+|email|varchar(32)|否|
+|pass|varchar(32)|否|
+|passwd|varchar(16)|否|
+|t|int(11)|否|0
+|u|bigint(20)|否|
+|d|bigint(20)|否|
+|transfer_enable|bigint(20)|否|
+|//**port**//|int(11)|否|
+|switch|tinyint(4)|否|1
+|enable|tinyint(4)|否|1
+|type|tinyint(4)|否|1
+|last_get_gitf_time|int(11)|否|0
+|last_rest_pass_time|int(11)|否|0
+|vpnuser|varchar(20)|否|
+|vpnpass|varchar(20)|否|
+|endtime|date|否|
