ad5c8835498221e68e552e1aed6b5f6691bb9917
Someant <594272732@qq.com>
2015-02-10 14:48:24 +0800

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2015 年 02 月 10 日 06:27
+-- 生成日期: 2015 年 02 月 10 日 06:47
 -- 服务器版本: 5.5.24-log
 -- PHP 版本: 5.4.3
 
@@ -19,6 +19,30 @@ SET time_zone = "+00:00";
 --
 -- 数据库: `shad`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `gift`
+--
+
+CREATE TABLE IF NOT EXISTS `gift` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(10) NOT NULL,
+  `usetime` datetime NOT NULL,
+  `useid` int(11) NOT NULL,
+  `type` int(11) NOT NULL DEFAULT '0',
+  `status` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- 转存表中的数据 `gift`
+--
+
+INSERT INTO `gift` (`Id`, `number`, `usetime`, `useid`, `type`, `status`) VALUES
+(1, '234331', '2015-02-04 05:03:28', 0, 0, 0),
+(2, '111111', '2015-02-08 07:36:12', 0, 0, 0);
 
 -- --------------------------------------------------------
 
