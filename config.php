13f9d8c9a05d42cfa4a690feb13a14c7a3970202
Someant <594272732@qq.com>
2015-02-10 13:16:21 +0800

    config page

---
+++ config.php
@@ -0,0 +1,9 @@
+﻿<?php
+//数据库配置文件
+define('DBHost', 'localhost');
+define('DBName', 'xxxx');
+define('DBUser', 'xxxx');
+define('DBPassword', 'xxxxxxx');
+require(dirname(__FILE__)."/src/PDO.class.php");
+$DB = new Db(DBHost, DBName, DBUser, DBPassword);
+?>
