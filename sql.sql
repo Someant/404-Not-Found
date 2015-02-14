b43a535816ab469bb57b7b6a3ed1bf3be6168016
Someant <594272732@qq.com>
2015-02-14 21:17:23 +0800

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2015 年 02 月 10 日 06:47
+-- 生成日期: 2015 年 02 月 14 日 13:16
 -- 服务器版本: 5.5.24-log
 -- PHP 版本: 5.4.3
 
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `gift` (
 
 INSERT INTO `gift` (`Id`, `number`, `usetime`, `useid`, `type`, `status`) VALUES
 (1, '234331', '2015-02-04 05:03:28', 0, 0, 0),
-(2, '111111', '2015-02-08 07:36:12', 0, 0, 0);
+(2, '111111', '2015-02-11 11:26:05', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -64,12 +64,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `enable` tinyint(4) NOT NULL DEFAULT '1',
   `type` tinyint(4) NOT NULL DEFAULT '1',
   `last_get_gitf_time` int(11) NOT NULL DEFAULT '0',
-  `last_rest_pass_time` int(11) NOT NULL DEFAULT '0',
+  `last_rest_pass_time` datetime NOT NULL,
   `vpnuser` varchar(20) NOT NULL,
   `vpnpass` varchar(20) NOT NULL,
   `endtime` date NOT NULL,
   PRIMARY KEY (`id`,`port`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=39 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `pass`, `passwd`, `t`, `u`, `d`, `transfer_enable`, `port`, `switch`, `enable`, `type`, `last_get_gitf_time`, `last_rest_pass_time`, `vpnuser`, `vpnpass`, `endtime`) VALUES
+(1, 'test@test.com', '52fbe78a42e84cdbaf4f4e1d5100cf5a', '640554', 0, 0, 0, 10737418240, 50490, 1, 1, 7, 0, '2015-02-14 12:59:30', '', '', '0000-00-00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
