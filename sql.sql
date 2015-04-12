8c779f8c133f28c99d220a185762b3b7d3000d4a
Someant <594272732@qq.com>
2015-04-12 23:03:58 +0800

    Update sql

--- sql.sql
+++ sql.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- 主机: localhost
--- 生成日期: 2015 年 02 月 14 日 13:16
--- 服务器版本: 5.5.24-log
--- PHP 版本: 5.4.3
+-- Host: 127.0.0.1
+-- Generation Time: 2015-04-12 17:03:26
+-- 服务器版本： 5.6.17
+-- PHP Version: 5.5.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,8 +17,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- 数据库: `shad`
+-- Database: `404`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ad`
+--
+
+CREATE TABLE IF NOT EXISTS `ad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(20) NOT NULL,
+  `pass` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -29,20 +43,11 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `gift` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `number` varchar(10) NOT NULL,
-  `usetime` datetime NOT NULL,
-  `useid` int(11) NOT NULL,
-  `type` int(11) NOT NULL DEFAULT '0',
+  `usetime` datetime DEFAULT NULL,
+  `month` int(11) NOT NULL DEFAULT '1',
   `status` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- 转存表中的数据 `gift`
---
-
-INSERT INTO `gift` (`Id`, `number`, `usetime`, `useid`, `type`, `status`) VALUES
-(1, '234331', '2015-02-04 05:03:28', 0, 0, 0),
-(2, '111111', '2015-02-11 11:26:05', 0, 0, 0);
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
 
 -- --------------------------------------------------------
 
@@ -64,19 +69,12 @@ CREATE TABLE IF NOT EXISTS `user` (
   `enable` tinyint(4) NOT NULL DEFAULT '1',
   `type` tinyint(4) NOT NULL DEFAULT '1',
   `last_get_gitf_time` int(11) NOT NULL DEFAULT '0',
-  `last_rest_pass_time` datetime NOT NULL,
-  `vpnuser` varchar(20) NOT NULL,
-  `vpnpass` varchar(20) NOT NULL,
+  `last_rest_pass_time` datetime DEFAULT NULL,
   `endtime` date NOT NULL,
+  `signtime` datetime NOT NULL,
+  `level` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`,`port`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- 转存表中的数据 `user`
---
-
-INSERT INTO `user` (`id`, `email`, `pass`, `passwd`, `t`, `u`, `d`, `transfer_enable`, `port`, `switch`, `enable`, `type`, `last_get_gitf_time`, `last_rest_pass_time`, `vpnuser`, `vpnpass`, `endtime`) VALUES
-(1, 'test@test.com', '52fbe78a42e84cdbaf4f4e1d5100cf5a', '640554', 0, 0, 0, 10737418240, 50490, 1, 1, 7, 0, '2015-02-14 12:59:30', '', '', '0000-00-00');
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=43 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
