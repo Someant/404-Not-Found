7b09ccf6c88c3078d1c16bace67966acb5eff66c
carvenli <carvenli@users.noreply.github.com>
2015-08-27 17:54:23 +0800

    改路径为相对

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DBHost', 'localhost');
 define('DBName', '404');
 define('DBUser', '404');
 define('DBPassword', '123123');
-require(dirname(__FILE__)."/src/PDO.class.php");
+require(dirname(__FILE__)."./src/PDO.class.php");
 $DB = new Db(DBHost, DBName, DBUser, DBPassword);
 $beginflow=10240;//初始流量设置 单位MB
 $portwidth=[50000,59999]//端口范围
